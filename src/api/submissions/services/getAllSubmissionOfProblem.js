b86ceb0da4d89b20c7d8b0cd631f261d046a8d46
Jalay <jalaym825@gmail.com>
2025-04-04 19:07:11 +0530

    implement getSubmissionOfProblem API endpoint

---
+++ src/api/submissions/services/getAllSubmissionOfProblem.js
@@ -0,0 +1,53 @@
+const ApiError = require("@entities/ApiError");
+const ApiResponse = require("@entities/ApiResponse");
+const prisma = require("@utils/prisma");
+
+const getSubmissionOfProblem = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const userId = req.user.id;
+
+        const submissions = await prisma.submission.findMany({
+            where: {
+                problemId: id,
+                userId: userId,
+            },
+            select: {
+                id: true,
+                code: true,
+                language: true,
+                status: true,
+                createdAt: true,
+                testCaseResults: {
+                    select: {
+                        id: true,
+                        status: true,
+                        testCase: {
+                            select: {
+                                id: true,
+                                // input: true,
+                                // output: true,
+                                // explanation: true,
+                                isHidden: true,
+                                points: true,
+                            },
+                        },
+                    },
+                },
+            },
+        });
+
+        return res.status(200).json(new ApiResponse(submissions, "Submissions fetched successfully"));
+    } catch (error) {
+        return next(
+            new ApiError(
+                500,
+                error.message,
+                error,
+                `/submissions/getAllSubmissionOfProblem/${req.params.problemId}`
+            )
+        );
+    }
+};
+
+module.exports = getSubmissionOfProblem;
