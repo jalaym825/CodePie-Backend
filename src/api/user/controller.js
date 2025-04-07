c7caaba1740a2c22a623d413086d64b57d94dd46
Vatsal Bharkhada <vatsalbharkhada@gmail.com>
2025-04-07 23:06:41 +0530

    Add API to sync frontend time with server

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -1,7 +1,12 @@
+const ApiResponse = require('@entities/ApiResponse');
 const isAuthenticated = require('../../middlewares/isAuthenticated');
 const getUserProfile = require('./services/getUserProfile');
 const router = require('express').Router();
 
 router.get('/profile', isAuthenticated, getUserProfile);
+
+router.get("/api/time", (req, res) => {
+    res.json(new ApiResponse({ serverTime: Date.now() }, "Server time fetched successfully"));
+});
 
 module.exports = router;
