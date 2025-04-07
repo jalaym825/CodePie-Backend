986798d6dd819bb77cd009c442444bc5c7d70ea5
Jalay <jalaym825@gmail.com>
2025-04-07 11:48:48 +0530

    fix: correct order of parameters in ApiResponse for getMe function fix: remove unused explanation field from testCaseSchema in create-problem-dto

--- src/api/auth/services/getMe.js
+++ src/api/auth/services/getMe.js
@@ -3,7 +3,7 @@ const ApiError = require("@entities/ApiError");
 
 const getMe = async (req, res, next) => {
     try {
-        return res.json(new ApiResponse("User is logged in", req.user));
+        return res.json(new ApiResponse(req.user, "User is logged in"));
     } catch (err) {
         next(new ApiError(400, err.message, err, '/auth/me'));
     }
